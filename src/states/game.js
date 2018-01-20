3106ed5f64d8a7fba1468f5693182ad28e6e1664
Cassandra Le <cassandrale@Cassandras-MBP.fios-router.home>
2018-01-20 13:23:11 -0500

    DONE: add moving background

--- src/states/game.js
+++ src/states/game.js
@@ -2,23 +2,30 @@ import * as firebase from 'firebase';
 
 class Game extends Phaser.State {
 
+    //---------------- INITIALIZE FIREBASE APP -------------------
     constructor() {
         super();
         firebase.initializeApp({
-            apiKey: "AIzaSyCTpeoii5DHFeaaw6OK6j68Q6d_WW4fuy8",
-            authDomain: "newagent-f3b16.firebaseapp.com",
-            databaseURL: "https://newagent-f3b16.firebaseio.com",
-            projectId: "newagent-f3b16",
-            storageBucket: "newagent-f3b16.appspot.com",
-            messagingSenderId: "469349041880"
+         apiKey: "AIzaSyBHKXbK6D4nZefsDB8Nt3pW6DNae_JgJsQ",
+         authDomain: "pennapps2018.firebaseapp.com",
+         databaseURL: "https://pennapps2018.firebaseio.com",
+         projectId: "pennapps2018",
+         storageBucket: "",
+         messagingSenderId: "286224470364"
         });
     }
 
+    //--------------- LOAD IMAGE OF SHIP --------
     preload(){
         this.game.load.image('ship', 'assets/sprites/spaceship.png');
+        this.game.load.image('bg', 'assets/background.jpg')
     }
 
     create() {
+
+      this.starfield = this.game.add.tileSprite(0, 0, 1000, 800, 'background');
+
+      //---------- STORE A LIST OF PEOPLE AS PLAYERS ------------
         this.players = {};
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.player = this.game.add.sprite(200, 200, 'ship');
@@ -33,6 +40,7 @@ class Game extends Phaser.State {
 
         this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
 
+        //-------------- STORE PLAYER'S ID AS KEY AND POSITION AS VALUE ------------
         firebase.database().ref('players').on('child_added', child => {
             if(child.key !== this.player.key) {
                 const p = this.game.add.sprite(child.val().x, child.val().y, 'ship');
@@ -49,6 +57,8 @@ class Game extends Phaser.State {
     }
 
     update() {
+        this.starfield.tilePosition.y += 2;
+        this.starfield.tilePosition.x += 2;
         this.game.physics.arcade.moveToPointer(this.player, 60, this.game.input.activePointer, 500);
         // console.log(this.player.position);
         firebase.database().ref('players').child(this.player.key).set(this.player.position)
@@ -65,6 +75,7 @@ class Game extends Phaser.State {
         this.game.state.start('gameover');
     }
 
+    //----------------------------- GENERATE RANDOM ID --------------------------
     uuidv4() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -72,6 +83,7 @@ class Game extends Phaser.State {
         });
     }
 
+    //------------------ RANODMLY GENERATE FOOD -------------------
     makeFood() {
         const x = this.game.rnd.integerInRange(0, this.game.width);
         const y = this.game.rnd.integerInRange(0, this.game.height);
